bd253dbbed14e6bde420ea888f699cea474e2e19
Patrick Stevens <3138005+Smaug123@users.noreply.github.com>
2025-07-18 15:37:37 +0100

    Add fsi file around TimingWheel (#3)

--- WoofWare.TimingWheel/TimingWheel.fs
+++ WoofWare.TimingWheel/TimingWheel.fs
@@ -15,71 +15,73 @@ type TimingWheel<'a> =
 [<RequireQualifiedAccess>]
 module TimingWheel =
 
-    let internal config (t : TimingWheel<'a>) : Config = t.Config
+    let internal config<'a> (t : TimingWheel<'a>) : Config = t.Config
 
-    let alarmPrecision (t : TimingWheel<'a>) : TimeNs.Span = Config.alarmPrecision t.Config
+    let alarmPrecision<'a> (t : TimingWheel<'a>) : TimeNs.Span = Config.alarmPrecision t.Config
 
     /// The earliest time that ever existed for this TimingWheel.
-    let start (t : TimingWheel<'a>) : TimeNs = t.Start
+    let start<'a> (t : TimingWheel<'a>) : TimeNs = t.Start
 
     /// The current time, according to this TimingWheel. For example, you can't register alarms for before this time,
     /// because that's in the past.
-    let now (t : TimingWheel<'a>) : TimeNs = t.Now
+    let now<'a> (t : TimingWheel<'a>) : TimeNs = t.Now
 
-    let maxIntervalNum (t : TimingWheel<'a>) : IntervalNum = t.MaxIntervalNum
+    let maxIntervalNum<'a> (t : TimingWheel<'a>) : IntervalNum = t.MaxIntervalNum
 
     /// The latest time you can set an alarm for. (We don't have enough precision to register alarms arbitrarily far
     /// in the future, for example.)
-    let maxAllowedAlarmTime (t : TimingWheel<'a>) : TimeNs = t.MaxAllowedAlarmTime
+    let maxAllowedAlarmTime<'a> (t : TimingWheel<'a>) : TimeNs = t.MaxAllowedAlarmTime
 
     type Alarm = Elt
 
+    [<RequireQualifiedAccess>]
     module Alarm =
         let null' : Alarm = Elt.null'
-        let atTime (tw : TimingWheel<ExternalEltValue<'a>>) (t : ExternalElt) : TimeNs = Elt.at tw.PriorityQueue t
+        let atTime<'a> (tw : TimingWheel<ExternalEltValue<'a>>) (t : ExternalElt) : TimeNs = Elt.at tw.PriorityQueue t
         let value<'a> (tw : TimingWheel<ExternalEltValue<'a>>) (t : ExternalElt) : 'a = Elt.value tw.PriorityQueue t
-        let intervalNum (tw : TimingWheel<ExternalEltValue<'a>>) (t : ExternalElt) : Key = Elt.key tw.PriorityQueue t
 
+        let intervalNum<'a> (tw : TimingWheel<ExternalEltValue<'a>>) (t : ExternalElt) : Key =
+            Elt.key tw.PriorityQueue t
 
-    let iter (t : TimingWheel<ExternalEltValue<'a>>) (f : Elt -> unit) : unit = PriorityQueue.iter t.PriorityQueue f
+    let iter<'a> (t : TimingWheel<ExternalEltValue<'a>>) (f : Elt -> unit) : unit = PriorityQueue.iter t.PriorityQueue f
 
-    let length (t : TimingWheel<'a>) : int = t.PriorityQueue.Length
-    let isEmpty (t : TimingWheel<'a>) : bool = length t = 0
+    let length<'a> (t : TimingWheel<'a>) : int = t.PriorityQueue.Length
+    let isEmpty<'a> (t : TimingWheel<'a>) : bool = length t = 0
 
-    let raiseNextAlarmFiresAtThrowingOfEmptyTimingWheel () =
+    let private raiseNextAlarmFiresAtThrowingOfEmptyTimingWheel () =
         failwith "nextAlarmFiresAtThrowing of empty timing wheel"
 
-    let raiseNextAlarmFiresAtWithAllAlarmsInMaxInterval () =
+    let private raiseNextAlarmFiresAtWithAllAlarmsInMaxInterval () =
         failwith "nextAlarmFiresAtThrowing with all alarms in max interval"
 
-    let internal pool (t : TimingWheel<'a>) : Pool<'a> = t.PriorityQueue.Pool
+    let internal pool<'a> (t : TimingWheel<'a>) : Pool<'a> = t.PriorityQueue.Pool
 
     let intervalNumInternal (time : TimeNs) (alarmPrecision : AlarmPrecision) : IntervalNum =
         IntervalNum.ofInt64 (AlarmPrecision.intervalNum alarmPrecision time)
 
-    let intervalNumUnchecked (t : TimingWheel<'a>) (time : TimeNs) : IntervalNum =
+    let intervalNumUnchecked<'a> (t : TimingWheel<'a>) (time : TimeNs) : IntervalNum =
         intervalNumInternal time t.Config.AlarmPrecision
 
-    let minTime = TimeNs.epoch
+    let minTime : TimeNs = TimeNs.epoch
     let maxTime : TimeNs = TimeNs.maxValueRepresentable
-    let minIntervalNum = IntervalNum.zero
+    let minIntervalNum : IntervalNum = IntervalNum.zero
 
-    let intervalNum (t : TimingWheel<'a>) (time : TimeNs) : IntervalNum =
+    let intervalNum<'a> (t : TimingWheel<'a>) (time : TimeNs) : IntervalNum =
         if time < minTime then
-            failwith $"intervalNum got time too far in the past: {time}, min is {minTime}"
+            invalidArg "time" $"intervalNum got time too far in the past: {time}, min is {minTime}"
 
         intervalNumUnchecked t time
 
-    let intervalNumStartUnchecked (t : TimingWheel<'a>) (intervalNum : IntervalNum) : TimeNs =
+    let intervalNumStartUnchecked<'a> (t : TimingWheel<'a>) (intervalNum : IntervalNum) : TimeNs =
         AlarmPrecision.intervalNumStart t.Config.AlarmPrecision (intervalNum |> IntervalNum.toInt64)
 
-    let raiseIntervalNumStartGotTooSmall (intervalNum : IntervalNum) : unit =
+    let private raiseIntervalNumStartGotTooSmall (intervalNum : IntervalNum) : unit =
         failwith $"intervalNumStart got too small intervalNum: {intervalNum}, min was {minIntervalNum}"
 
-    let raiseIntervalNumStartGotTooLarge (t : TimingWheel<'a>) (intervalNum : IntervalNum) : unit =
+    let private raiseIntervalNumStartGotTooLarge (t : TimingWheel<'a>) (intervalNum : IntervalNum) : unit =
         failwith $"intervalNumStart got too large intervalNum: {intervalNum}, max was {t.MaxIntervalNum}"
 
-    let intervalNumStart (t : TimingWheel<'a>) (intervalNum : IntervalNum) : TimeNs =
+    let intervalNumStart<'a> (t : TimingWheel<'a>) (intervalNum : IntervalNum) : TimeNs =
         if intervalNum < minIntervalNum then
             raiseIntervalNumStartGotTooSmall intervalNum
 
@@ -88,7 +90,7 @@ module TimingWheel =
 
         intervalNumStartUnchecked t intervalNum
 
-    let nextAlarmFiresAtInternal (t : TimingWheel<'a>) (key : Key) : TimeNs =
+    let nextAlarmFiresAtInternal<'a> (t : TimingWheel<'a>) (key : Key) : TimeNs =
         (* [interval_num_start t key] is the key corresponding to the start of the time interval
      holding the first alarm in [t].  Advancing to that would not be enough, since the
      alarms in that interval don't fire until the clock is advanced to the start of the
@@ -96,7 +98,7 @@ module TimingWheel =
      interval. *)
         intervalNumStart t (Key.succ key)
 
-    let nextAlarmFiresAt (t : TimingWheel<ExternalEltValue<'a>>) : TimeNs option =
+    let nextAlarmFiresAt<'a> (t : TimingWheel<ExternalEltValue<'a>>) : TimeNs option =
         let elt = PriorityQueue.minElt' t.PriorityQueue
 
         if InternalElt.isNull elt then
@@ -110,7 +112,7 @@ module TimingWheel =
         else
             Some (nextAlarmFiresAtInternal t key)
 
-    let nextAlarmFiresAtThrowing (t : TimingWheel<ExternalEltValue<'a>>) : TimeNs =
+    let nextAlarmFiresAtThrowing<'a> (t : TimingWheel<ExternalEltValue<'a>>) : TimeNs =
         let elt = PriorityQueue.minElt' t.PriorityQueue
 
         if InternalElt.isNull elt then
@@ -123,7 +125,7 @@ module TimingWheel =
 
         nextAlarmFiresAtInternal t key
 
-    let computeMaxAllowedAlarmTime t : TimeNs =
+    let computeMaxAllowedAlarmTime<'a> (t : TimingWheel<'a>) : TimeNs =
         let maxAllowedKey = PriorityQueue.maxAllowedKey t.PriorityQueue in
 
         if maxAllowedKey >= t.MaxIntervalNum then
@@ -133,16 +135,16 @@ module TimingWheel =
                 (intervalNumStartUnchecked t maxAllowedKey)
                 ((AlarmPrecision.toSpan t.Config.AlarmPrecision) - TimeNs.Span.nanosecond)
 
-    let nowIntervalNum (t : TimingWheel<'a>) : Key =
+    let nowIntervalNum<'a> (t : TimingWheel<'a>) : Key =
         PriorityQueue.minAllowedKey t.PriorityQueue
 
-    let minAllowedAlarmIntervalNum = nowIntervalNum
-    let maxAllowedAlarmIntervalNum (t : TimingWheel<'a>) : IntervalNum = intervalNum t t.MaxAllowedAlarmTime
+    let minAllowedAlarmIntervalNum<'a> (t : TimingWheel<'a>) : Key = nowIntervalNum t
+    let maxAllowedAlarmIntervalNum<'a> (t : TimingWheel<'a>) : IntervalNum = intervalNum t t.MaxAllowedAlarmTime
 
-    let intervalStart (t : TimingWheel<'a>) (time : TimeNs) : TimeNs =
+    let intervalStart<'a> (t : TimingWheel<'a>) (time : TimeNs) : TimeNs =
         intervalNumStartUnchecked t (intervalNum t time)
 
-    let invariant (inv : 'a -> unit) (t : TimingWheel<ExternalEltValue<'a>>) : unit =
+    let invariant<'a> (inv : 'a -> unit) (t : TimingWheel<ExternalEltValue<'a>>) : unit =
         Config.invariant t.Config
 
         do
@@ -190,7 +192,7 @@ module TimingWheel =
                     failwith "alarm started in the past"
             )
 
-    let advanceClock
+    let advanceClock<'a>
         (t : TimingWheel<ExternalEltValue<'a>>)
         (toTime : TimeNs)
         (handleFired : ExternalElt -> unit)
@@ -210,7 +212,12 @@ module TimingWheel =
             | PriorityQueue.IncreaseMinAllowedKeyResult.MaxAllowedKeyMaybeChanged ->
                 t.MaxAllowedAlarmTime <- computeMaxAllowedAlarmTime t
 
-    let advanceClockStopAtNextAlarm t toTime handleFired =
+    let advanceClockStopAtNextAlarm<'a>
+        (t : TimingWheel<ExternalEltValue<'a>>)
+        (toTime : TimeNs)
+        (handleFired : ExternalElt -> unit)
+        : unit
+        =
         let minElt = PriorityQueue.minElt' t.PriorityQueue
 
         if InternalElt.isNull minElt then
@@ -227,7 +234,7 @@ module TimingWheel =
 
     let create<'a> (config : Config) (start : TimeNs) : TimingWheel<ExternalEltValue<'a>> =
         if start < TimeNs.epoch then
-            failwith $"TimingWheel.create got start {start} before the epoch"
+            invalidArg "start" $"TimingWheel.create got start {start} before the epoch"
 
         let queue =
             PriorityQueue.create (config.Capacity |> Option.defaultValue 1) (Some config.LevelBits)
@@ -247,20 +254,20 @@ module TimingWheel =
         advanceClock t start (fun _ -> failwith "no firing")
         t
 
-    let addAtIntervalNum (t : TimingWheel<ExternalEltValue<'a>>) (at : Key) (value : 'a) : ExternalElt =
-        let numStart = intervalNumStart t at
+    let addAtIntervalNum<'a> (t : TimingWheel<ExternalEltValue<'a>>) (atTime : Key) (value : 'a) : ExternalElt =
+        let numStart = intervalNumStart t atTime
 
-        PriorityQueue.internalAdd t.PriorityQueue at numStart value
+        PriorityQueue.internalAdd t.PriorityQueue atTime numStart value
         |> InternalElt.toExternal
 
-    let raiseThatFarInTheFuture (t : TimingWheel<'a>) (at : TimeNs) : unit =
+    let private raiseThatFarInTheFuture (t : TimingWheel<'a>) (at : TimeNs) : unit =
         failwith $"TimingWheel cannot schedule alarm that far in the future (max: {t.MaxAllowedAlarmTime}; got: {at})"
 
-    let raiseBeforeStartOfCurrentInterval (t : TimingWheel<'a>) (at : TimeNs) : unit =
+    let private raiseBeforeStartOfCurrentInterval (t : TimingWheel<'a>) (at : TimeNs) : unit =
         failwith
             $"TimingWheel cannot schedule alarm for {at} before start of current interval ({t.NowIntervalNumStart})"
 
-    let ensureCanScheduleAlarm t atTime =
+    let ensureCanScheduleAlarm<'a> (t : TimingWheel<'a>) (atTime : TimeNs) : unit =
         if atTime > t.MaxAllowedAlarmTime then
             raiseThatFarInTheFuture t atTime
 
@@ -271,13 +278,13 @@ module TimingWheel =
         ensureCanScheduleAlarm t atTime
         InternalElt.toExternal (PriorityQueue.internalAdd t.PriorityQueue (intervalNumUnchecked t atTime) atTime value)
 
-    let remove (t : TimingWheel<ExternalEltValue<'a>>) (alarm : ExternalElt) : unit =
+    let remove<'a> (t : TimingWheel<ExternalEltValue<'a>>) (alarm : ExternalElt) : unit =
         PriorityQueue.remove t.PriorityQueue alarm
 
-    let clear (t : TimingWheel<ExternalEltValue<'a>>) : unit = PriorityQueue.clear t.PriorityQueue
-    let mem (t : TimingWheel<'a>) (alarm : ExternalElt) : bool = PriorityQueue.mem t.PriorityQueue alarm
+    let clear<'a> (t : TimingWheel<ExternalEltValue<'a>>) : unit = PriorityQueue.clear t.PriorityQueue
+    let mem<'a> (t : TimingWheel<'a>) (alarm : ExternalElt) : bool = PriorityQueue.mem t.PriorityQueue alarm
 
-    let rescheduleGen
+    let rescheduleGen<'a>
         (t : TimingWheel<ExternalEltValue<'a>>)
         (alarm : ExternalElt)
         (key : Key)
@@ -290,13 +297,13 @@ module TimingWheel =
         ensureCanScheduleAlarm t atTime
         PriorityQueue.change t.PriorityQueue alarm key atTime
 
-    let reschedule (t : TimingWheel<ExternalEltValue<'a>>) (alarm : ExternalElt) (atTime : TimeNs) : unit =
+    let reschedule<'a> (t : TimingWheel<ExternalEltValue<'a>>) (alarm : ExternalElt) (atTime : TimeNs) : unit =
         rescheduleGen t alarm (intervalNumUnchecked t atTime) atTime
 
-    let rescheduleAtIntervalNum (t : TimingWheel<ExternalEltValue<'a>>) (alarm : ExternalElt) (atTime : Key) : unit =
-        rescheduleGen t alarm atTime (intervalNumStart t atTime)
+    let rescheduleAtIntervalNum<'a> (t : TimingWheel<ExternalEltValue<'a>>) (alarm : ExternalElt) (at : Key) : unit =
+        rescheduleGen t alarm at (intervalNumStart t at)
 
-    let minAlarmIntervalNum (t : TimingWheel<ExternalEltValue<'a>>) : Key option =
+    let minAlarmIntervalNum<'a> (t : TimingWheel<ExternalEltValue<'a>>) : Key option =
         let elt = PriorityQueue.minElt' t.PriorityQueue in
 
         if InternalElt.isNull elt then
@@ -304,7 +311,7 @@ module TimingWheel =
         else
             Some (InternalElt.key (pool t) elt)
 
-    let minAlarmIntervalNumThrowing (t : TimingWheel<ExternalEltValue<'a>>) : Key =
+    let minAlarmIntervalNumThrowing<'a> (t : TimingWheel<ExternalEltValue<'a>>) : Key =
         let elt = PriorityQueue.minElt' t.PriorityQueue
 
         if InternalElt.isNull elt then
@@ -312,15 +319,15 @@ module TimingWheel =
         else
             InternalElt.key (pool t) elt
 
-    let maxAlarmTimeInList (t : TimingWheel<ExternalEltValue<'a>>) (elt : InternalElt) : TimeNs =
+    let maxAlarmTimeInList<'a> (t : TimingWheel<ExternalEltValue<'a>>) (elt : InternalElt) : TimeNs =
         let pool = pool t
         InternalElt.maxAlarmTime pool elt (InternalElt.key pool elt)
 
-    let minAlarmTimeInList (t : TimingWheel<ExternalEltValue<'a>>) (elt : InternalElt) : TimeNs =
+    let minAlarmTimeInList<'a> (t : TimingWheel<ExternalEltValue<'a>>) (elt : InternalElt) : TimeNs =
         let pool = pool t
         InternalElt.minAlarmTime pool elt (InternalElt.key pool elt)
 
-    let maxAlarmTimeInMinInterval (t : TimingWheel<ExternalEltValue<'a>>) : TimeNs option =
+    let maxAlarmTimeInMinInterval<'a> (t : TimingWheel<ExternalEltValue<'a>>) : TimeNs option =
         let elt = PriorityQueue.minElt' t.PriorityQueue in
 
         if InternalElt.isNull elt then
@@ -328,7 +335,7 @@ module TimingWheel =
         else
             Some (maxAlarmTimeInList t elt)
 
-    let minAlarmTimeInMinInterval (t : TimingWheel<ExternalEltValue<'a>>) : TimeNs option =
+    let minAlarmTimeInMinInterval<'a> (t : TimingWheel<ExternalEltValue<'a>>) : TimeNs option =
         let elt = PriorityQueue.minElt' t.PriorityQueue in
 
         if InternalElt.isNull elt then
@@ -336,7 +343,7 @@ module TimingWheel =
         else
             Some (minAlarmTimeInList t elt)
 
-    let maxAlarmTimeInMinIntervalThrowing (t : TimingWheel<ExternalEltValue<'a>>) : TimeNs =
+    let maxAlarmTimeInMinIntervalThrowing<'a> (t : TimingWheel<ExternalEltValue<'a>>) : TimeNs =
         let elt = PriorityQueue.minElt' t.PriorityQueue in
 
         if InternalElt.isNull elt then
@@ -344,7 +351,7 @@ module TimingWheel =
 
         maxAlarmTimeInList t elt
 
-    let minAlarmTimeInMinIntervalThrowing (t : TimingWheel<ExternalEltValue<'a>>) : TimeNs =
+    let minAlarmTimeInMinIntervalThrowing<'a> (t : TimingWheel<ExternalEltValue<'a>>) : TimeNs =
         let elt = PriorityQueue.minElt' t.PriorityQueue
 
         if InternalElt.isNull elt then
@@ -352,5 +359,5 @@ module TimingWheel =
 
         minAlarmTimeInList t elt
 
-    let firePastAlarms (t : TimingWheel<ExternalEltValue<'a>>) (handleFired : ExternalElt -> unit) : unit =
+    let firePastAlarms<'a> (t : TimingWheel<ExternalEltValue<'a>>) (handleFired : ExternalElt -> unit) : unit =
         PriorityQueue.firePastAlarms t.PriorityQueue handleFired (nowIntervalNum t) t.Now
